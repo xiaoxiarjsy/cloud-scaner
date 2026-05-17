a32e42bb8f4ee7c27939e26396937524ed8978b0
mimanchi412 <1915992742@qq.com>
2026-05-18 01:41:51 +0800

    Allow browser admin initialization

--- worker/src/routes/init.routes.ts
+++ worker/src/routes/init.routes.ts
@@ -1,13 +1,21 @@
 import { Hono } from 'hono'
+import type { Context } from 'hono'
 import type { AppVariables, Env } from '../types/env'
 import { ensureDefaultAdmin } from '../services/bootstrap.service'
 import { ok } from '../utils/response'
 
 const initRoutes = new Hono<{ Bindings: Env; Variables: AppVariables }>()
 
-initRoutes.post('/', async (c) => {
+type InitContext = Context<{ Bindings: Env; Variables: AppVariables }>
+
+async function initialize(c: InitContext) {
   const result = await ensureDefaultAdmin(c.env)
   return ok(c, { initialized: true, ...result })
-})
+}
+
+initRoutes.get('/', initialize)
+initRoutes.post('/', initialize)
+initRoutes.get('', initialize)
+initRoutes.post('', initialize)
 
 export { initRoutes }
